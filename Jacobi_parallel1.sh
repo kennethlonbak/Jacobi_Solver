9df85343abbc89552a11ce6d9f592611ca94bffe
Kenneth Loenbaek <kennethlonbak@hotmail.com>
2018-01-11 23:58:00 +0100

    Small update of bash scripts to have loops

--- Jacobi_parallel1.sh
+++ Jacobi_parallel1.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=1
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=2
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=4
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=6
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=12
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=16
-JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=24
+n=${4:-7}
+N_th=(1 2 4 6 12 16 24)
+for ((i=1;i <=$n;i++))
+do
+    JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal1$3 show_state=.false. solver_type=3 N_th=${N_th[$i-1]}
+done
+
