879d3d0523a4b5b6f319198c28ef015d3ffeb47a
Kenneth Loenbaek <kennethlonbak@hotmail.com>
2018-01-12 11:40:48 +0100

    Small changes to bash file - troble to run on cluster

--- Jacobi_parallel2.sh
+++ Jacobi_parallel2.sh
@@ -3,5 +3,5 @@ n=${4:-7}
 N_th=(1 2 4 6 12 16 24)
 for ((i=1;i <=$n;i++))
 do
-    OMP_WAIT_POLICY=ACTIVE JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=pal2$3 show_state=.false. write_mat=.false. solver_type=4 N_th=${N_th[$i-1]}
+    OMP_WAIT_POLICY=ACTIVE JACOBI_SOLVER N=$1 d_min=1e-20 k_max=$2 filesub=$5pal2$3 show_state=.false. write_mat=.false. solver_type=4 N_th=${N_th[$i-1]}
 done
